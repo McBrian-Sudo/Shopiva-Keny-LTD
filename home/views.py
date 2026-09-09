7842a0bfbc8e9f84843b027d881a863206912d3f
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-10 01:10:14 +0300

    Add customer authentication imports

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from decimal import Decimal
 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.paginator import Paginator
 from django.db import transaction
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import CustomerRegistrationForm
 from .models import Order, OrderItem, Product
-
 
 def home(request):
     products = Product.objects.filter(is_active=True).order_by("-id")
