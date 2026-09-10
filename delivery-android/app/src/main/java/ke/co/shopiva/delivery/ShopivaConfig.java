1a3dcd971b03e6b922d89283388bfa9e1ba427ec
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-10 14:13:33 +0300

    Simplify Shopiva Admin Android configuration

--- delivery-android/app/src/main/java/ke/co/shopiva/delivery/ShopivaConfig.java
+++ delivery-android/app/src/main/java/ke/co/shopiva/delivery/ShopivaConfig.java
@@ -3,9 +3,5 @@ package ke.co.shopiva.delivery;
 public final class ShopivaConfig {
     private ShopivaConfig() {}
 
-    // Production Shopiva service.
     public static final String BASE_URL = "https://shopiva-keny-ltd.onrender.com";
-    public static final String LOCATION_ENDPOINT = BASE_URL + "/delivery/location/ping/";
-    public static final long UPDATE_INTERVAL_MS = 20_000L;
-    public static final long FASTEST_INTERVAL_MS = 10_000L;
 }
