b5a1d3b7e15833e6bd3f7745fbf3409bae7f2356
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-10 14:23:38 +0300

    Move Shopiva Admin configuration into the admin package

---
+++ delivery-android/app/src/main/java/ke/co/shopiva/admin/ShopivaConfig.java
@@ -0,0 +1,7 @@
+package ke.co.shopiva.admin;
+
+public final class ShopivaConfig {
+    private ShopivaConfig() {}
+
+    public static final String BASE_URL = "https://shopiva-keny-ltd.onrender.com";
+}
