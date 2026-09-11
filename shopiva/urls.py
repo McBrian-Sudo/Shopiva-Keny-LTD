c5af8defca573bd2bb96dfe41493aa308d227043
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-11 19:26:21 +0300

    Keep product media available after production hardening

--- shopiva/urls.py
+++ shopiva/urls.py
@@ -86,5 +86,4 @@ urlpatterns = [
     path("admin/", shopiva_admin_site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
