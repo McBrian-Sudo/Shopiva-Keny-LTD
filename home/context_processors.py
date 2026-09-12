78025a417e4e6662cffdce9e83d3dfc5d1239ca4
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-12 16:55:47 +0300

    Expose Google Maps API key and Map ID to templates

--- home/context_processors.py
+++ home/context_processors.py
@@ -2,4 +2,7 @@ from django.conf import settings
 
 
 def google_maps(request):
-    return {"google_maps_api_key": getattr(settings, "GOOGLE_MAPS_API_KEY", "")}
+    return {
+        "google_maps_api_key": getattr(settings, "GOOGLE_MAPS_API_KEY", ""),
+        "google_maps_map_id": getattr(settings, "GOOGLE_MAPS_MAP_ID", ""),
+    }
