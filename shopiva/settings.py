e9402fd499fcd5d04c94f44fec85ca0e143b4591
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-12 18:20:52 +0300

    Enable Shopiva SEO middleware

--- shopiva/settings.py
+++ shopiva/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "home.seo_middleware.ShopivaSeoMiddleware",
 ]
 
 ROOT_URLCONF = "shopiva.urls"
