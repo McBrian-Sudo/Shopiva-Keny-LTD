6b9a56216c422af1e841d046b758aeabad0ed9ed
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-15 00:21:44 +0300

    Use full live delivery command center in admin

--- home/google_admin_map.py
+++ home/google_admin_map.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 @staff_member_required(login_url="admin_login")
 def google_admin_delivery_map(request):
-    return render(request, "admin/google_delivery_map.html")
+    return render(request, "admin/delivery_command_center.html")
