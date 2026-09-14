bd3df1f1692ae6b2a1af4d248856cec2fb6458f4
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-15 01:38:03 +0300

    Force fresh Shopping homepage responses

--- shopiva/settings.py
+++ shopiva/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "home.homepage_cache_buster.ShopivaHomepageCacheBusterMiddleware",
     "home.seo_middleware.ShopivaSeoMiddleware",
     "home.branding_middleware.ShopivaBrandingMiddleware",
 ]
