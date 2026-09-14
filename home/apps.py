0749a5ef7a872e4d0bcbc3ee52ce992c59c63586
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-14 15:09:02 +0300

    Register Shopiva product media automation signals

--- home/apps.py
+++ home/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     name = "home"
+
+    def ready(self):
+        # Register product-media signals without changing existing marketplace flows.
+        from . import media_signals  # noqa: F401
