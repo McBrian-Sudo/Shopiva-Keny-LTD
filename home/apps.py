94b71bdcee3cfff60d74df9bb1449955b21b21a0
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-15 15:39:59 +0300

    Register seller order notification signals

--- home/apps.py
+++ home/apps.py
@@ -5,5 +5,6 @@ class HomeConfig(AppConfig):
     name = "home"
 
     def ready(self):
-        # Register product-media signals without changing existing marketplace flows.
+        # Register product-media and order-notification signals.
         from . import media_signals  # noqa: F401
+        from . import order_notifications  # noqa: F401
