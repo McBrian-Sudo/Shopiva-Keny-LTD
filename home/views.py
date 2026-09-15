035d38fc24cfcdd01087ff1bb32b60ad13566b75
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-15 13:05:43 +0300

    Correct ProductMedia import without moving forms

--- home/views.py
+++ home/views.py
@@ -16,7 +16,8 @@ from django.utils import timezone
 logger = logging.getLogger(__name__)
 
 from .forms import CustomerRegistrationForm
-from .models_product_media import ProductMedia, SellerRegistrationForm, SellerProductForm, ProductReviewForm
+from .models_product_media import ProductMedia
+from .forms import CustomerRegistrationForm, SellerRegistrationForm, SellerProductForm, ProductReviewForm
 from .models import CustomerAddress, DeliveryAgent, DeliveryLocationPing, Order, OrderEvent, OrderItem, Product, ProductReview, SellerPayoutRequest, SellerProfile, SellerSettlement, SellerWallet, WishlistItem
 
 
