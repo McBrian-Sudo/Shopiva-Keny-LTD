911fc7e5da688afc7321f9bb3335655829e25aaa
McBrian-Sudo <markbriannjuguna@gmail.com>
2026-09-19 11:22:41 +0300

    Merge pricing and privacy migration branches

---
+++ home/migrations/0027_merge_pricing_and_privacy.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("home", "0018_order_pricing_breakdown"),
+        ("home", "0026_order_customer_privacy"),
+    ]
+
+    operations = []
